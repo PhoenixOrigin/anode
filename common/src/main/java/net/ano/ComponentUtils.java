c7cbd7bbada7c075e7fab97d96b8d3a1107075fe
nHexanol <nHexanol@gmail.com>
2022-12-31 17:16:13 +0000

    done

---
+++ common/src/main/java/net/ano/ComponentUtils.java
@@ -0,0 +1,121 @@
+package net.ano;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class ComponentUtils {
+
+    public static String getCoded(Component component) {
+        StringBuilder result = new StringBuilder();
+
+        component.visit(new CodedStringGenerator(result), Style.EMPTY);
+
+        return result.toString();
+    }
+
+    public static Optional<ChatFormatting> getChatFormatting(TextColor textColor) {
+        return getChatFormatting(textColor.getValue());
+    }
+
+    public static Optional<ChatFormatting> getChatFormatting(int textColor) {
+        return Arrays.stream(ChatFormatting.values())
+                .filter(c -> c.isColor() && textColor == c.getColor())
+                .findFirst();
+    }
+
+    private static StringBuilder tryConstructDifference(Style oldStyle, Style newStyle) {
+        StringBuilder add = new StringBuilder();
+
+        int oldColorInt = Optional.ofNullable(oldStyle.getColor())
+                .map(TextColor::getValue)
+                .orElse(-1);
+        int newColorInt = Optional.ofNullable(newStyle.getColor())
+                .map(TextColor::getValue)
+                .orElse(-1);
+
+        if (oldColorInt == -1) {
+            if (newColorInt != -1) {
+                getChatFormatting(newColorInt).ifPresent(add::append);
+            }
+        } else if (oldColorInt != newColorInt) {
+            return null;
+        }
+
+        if (oldStyle.isBold() && !newStyle.isBold()) return null;
+        if (!oldStyle.isBold() && newStyle.isBold()) add.append(ChatFormatting.BOLD);
+
+        if (oldStyle.isItalic() && !newStyle.isItalic()) return null;
+        if (!oldStyle.isItalic() && newStyle.isItalic()) add.append(ChatFormatting.ITALIC);
+
+        if (oldStyle.isUnderlined() && !newStyle.isUnderlined()) return null;
+        if (!oldStyle.isUnderlined() && newStyle.isUnderlined()) add.append(ChatFormatting.UNDERLINE);
+
+        if (oldStyle.isStrikethrough() && !newStyle.isStrikethrough()) return null;
+        if (!oldStyle.isStrikethrough() && newStyle.isStrikethrough()) add.append(ChatFormatting.STRIKETHROUGH);
+
+        if (oldStyle.isObfuscated() && !newStyle.isObfuscated()) return null;
+        if (!oldStyle.isObfuscated() && newStyle.isObfuscated()) add.append(ChatFormatting.OBFUSCATED);
+
+        return add;
+    }
+
+
+    private static class CodedStringGenerator implements FormattedText.StyledContentConsumer<Object> {
+        private final StringBuilder result;
+        Style oldStyle;
+
+        public CodedStringGenerator(StringBuilder result) {
+            this.result = result;
+            oldStyle = Style.EMPTY;
+        }
+
+        @Override
+        public Optional<Object> accept(Style style, String string) {
+            handleStyleDifference(oldStyle, style, result);
+            result.append(string);
+
+            oldStyle = style;
+
+            return Optional.empty();
+        }
+
+        /**
+         * This method handles the fact that the style likely has changed between 2 components
+         *
+         * <p>It tries to first generate a constructive way of adding color codes to get from the old
+         * style to the new style. If that does not succeed, it instead resets the format and adds the
+         * color codes of the new style
+         */
+        private static void handleStyleDifference(Style oldStyle, Style newStyle, StringBuilder result) {
+            if (oldStyle.equals(newStyle)) return;
+
+            if (!oldStyle.isEmpty()) {
+                StringBuilder different = tryConstructDifference(oldStyle, newStyle);
+
+                if (different != null) {
+                    result.append(different);
+                    return;
+                }
+            }
+
+            result.append(ChatFormatting.RESET);
+
+            if (newStyle.getColor() != null) {
+                getChatFormatting(newStyle.getColor()).ifPresent(result::append);
+            }
+
+            if (newStyle.isBold()) result.append(ChatFormatting.BOLD);
+            if (newStyle.isItalic()) result.append(ChatFormatting.ITALIC);
+            if (newStyle.isUnderlined()) result.append(ChatFormatting.UNDERLINE);
+            if (newStyle.isStrikethrough()) result.append(ChatFormatting.STRIKETHROUGH);
+            if (newStyle.isObfuscated()) result.append(ChatFormatting.OBFUSCATED);
+        }
+    }
+
+}
--- common/src/main/java/net/ano/EventListener.java
+++ common/src/main/java/net/ano/EventListener.java
@@ -3,12 +3,20 @@ package net.ano;
 import dev.architectury.event.CompoundEventResult;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.BossEvent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EventListener {
 
-    public static CompoundEventResult<Component> processChat(ChatType.Bound bound, Component component){
+    private static final Pattern pattern = Pattern.compile("/§3\\[WAR§3] The battle has begun!/gm");
+    private static boolean inWar = false;
+    public static CompoundEventResult<Component> processChat(ChatType.Bound bound, Component component) {
+        Matcher matcher = pattern.matcher(ComponentUtils.getCoded(component));
+        if (!matcher.matches()) return CompoundEventResult.pass();
+        inWar = true;
         return CompoundEventResult.pass();
-
     }
 
 }
