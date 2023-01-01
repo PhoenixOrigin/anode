42c1cb9f93c90523c11988f0d136d0116b7dd6cf
nHexanol <nHexanol@gmail.com>
2023-01-01 00:23:29 +0000

    help
    Took 1 hour 10 minutes

--- common/src/main/java/net/ano/mixin/ClientPacketListenerMixin.java
+++ common/src/main/java/net/ano/mixin/ClientPacketListenerMixin.java
@@ -5,20 +5,20 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.protocol.game.ClientboundPlayerChatPacket;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPacketListener.class)
-public class ClientPacketListenerMixin {
-
+public abstract class ClientPacketListenerMixin {
     @Inject(
             method = "handlePlayerChat",
             at =
             @At(
                     value = "INVOKE",
                     target =
-                            "Lnet/minecraft/client/multiplayer/chat/ChatListener;handlePlayerChatMessage(Lnet/minecraft/network/chat/PlayerChatMessage;Lcom/mojang/authlib/GameProfile;Lnet/minecraft/network/chat/ChatType$Bound;)V")
+                            "Lnet/minecraft/client/multiplayer/chat/ChatListener;handlePlayerChatMessage(Lnet/minecraft/network/chat/PlayerChatMessage;Lcom/mojang/authlib/GameProfile;Lnet/minecraft/network/chat/ChatType$Bound;)V"
+            )
     )
     private void handlePlayerChat(ClientboundPlayerChatPacket packet, CallbackInfo ci) {
         if (!Minecraft.getInstance().isSameThread()) return;
