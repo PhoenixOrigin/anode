30da3a5bc647ae5639c460e28ec2707603f57311
nHexanol <nHexanol@gmail.com>
2023-01-01 18:11:38 +0000

    IT WORKS
    Took 3 minutes

--- forge/src/main/java/net/ano/forge/anodeForge.java
+++ forge/src/main/java/net/ano/forge/anodeForge.java
@@ -1,12 +1,17 @@
 package net.ano.forge;
 
 import net.ano.anode;
+import net.minecraft.client.model.EntityModel;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
+
+import javax.swing.text.html.parser.Entity;
 
 @Mod(anode.MOD_ID)
 public class anodeForge {
@@ -16,6 +21,7 @@ public class anodeForge {
 
     @SubscribeEvent
     public static void preRenderPlayer(RenderPlayerEvent.Pre event){
-        GL12.glRotatef(180, event.getEntity().getBlockX(), event.getEntity().getBlockY(), event.getEntity().getBlockZ());
+        GL12.glRotated(180, event.getEntity().getBlockX(), event.getEntity().getBlockY(), event.getEntity().getBlockZ());
     }
+
 }
